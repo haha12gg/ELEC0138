68221fe1afe2a6bbc149bdc9156953011b695c85
Mikeyfas <96052557+Mikeyfas@users.noreply.github.com>
2024-04-10 15:09:21 +0100

    Update app_s_fake.py

--- csrf_attack/app_s_fake.py
+++ csrf_attack/app_s_fake.py
@@ -5,12 +5,10 @@ app1 = Flask(__name__)
 
 @app1.route('/changepassword')
 def csrf_demo():
-    # 假设 csrf.html 在这个应用的 templates 文件夹内
     return render_template('csrf_s.html')
 
 @app1.route('/change_password')
 def csrf_demo1():
-    # 假设 csrf.html 在这个应用的 templates 文件夹内
     return render_template('csrf_w.html')
 
 if __name__ == '__main__':
